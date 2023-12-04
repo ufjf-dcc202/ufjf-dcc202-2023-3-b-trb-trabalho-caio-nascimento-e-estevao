96505235a6ffb43a7addac3dfb14b9064d78ef3b
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-12-04 20:01:07 -0300

    corrige erros nas funcoes de definicao

--- src/Dados.js
+++ src/Dados.js
@@ -1,35 +1,30 @@
-let jog1= [(0)]
-let jog2= [(0)]
-let tab1= [(0,0,0),(0,0,0),(0,0,0)]; // dados do jogador 1 dentro da tabela
-let tab2= [(0,0,0),(0,0,0),(0,0,0)]; // dados do jogador 2 dentro da tabela
+let jog1 = 0;
+let jog2 = 0;
+let tab1 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]; // dados do jogador 1 dentro da tabela
+let tab2 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]; // dados do jogador 2 dentro da tabela
 
-function aleatorio1e6()
-{
-    return (Math.random() * (6 - 1) + 1);
+function aleatorio1e6() {
+    return Math.floor(Math.random() * 6) + 1;
 }
 
-function jogarDado(jog)
-{
-    jogador=aleatorio1e6();
+function jogarDado(jog) {
+    jog = aleatorio1e6();
+    return jog;
 }
 
-function definirDadoTabela(tab,posJtab,jog)
-{
-    posItab=0;
-    definiu=false;
-    while (definiu==false){
-        if (tab[posItab][posJtab]==0)
-        {
-            tab[posItab][posJtab]=jog;
-            definiu=true;
+function definirDadoTabela(tab, posJtab, jog) {
+    let posItab = 0;
+    let definiu = false;
+    while (definiu == false) {
+        if (tab[posItab][posJtab] == 0) {
+            tab[posItab][posJtab] = jog;
+            definiu = true;
+            jog = 0;
             return definiu;
-        }
-        else
-        {
+        } else {
             posItab++;
-            if (posItab==3)
-            {
-                definiu=false;
+            if (posItab == 3) {
+                definiu = false;
                 return definiu;
             }
         }
@@ -37,3 +32,6 @@ function definirDadoTabela(tab,posJtab,jog)
     // retorna definiu verdadeiro se a coluna nao estiver cheia
 }
 
+jog1 = jogarDado(jog1);
+definirDadoTabela(tab1, 0, jog1);
+console.log(`${tab1[0][0]}`);
