4960f8d6074b16ba584ed80ab12a519ec74e3325
Caio Nascimento <caio.nascimento@estudante.ufjf.br>
2023-12-04 21:20:07 -0300

    criando um novo arquivo

---
+++ main.js
@@ -0,0 +1,10 @@
+function valor() {
+
+    let dado = Math.floor(Math.random() * 6) + 1;
+    //Pega o botão que foi clicado e armazena na variável
+    //document.getElementById('tela').value += num
+
+    //Insere na tela um numero na frente do outro
+    document.getElementById('bot1x1').innerText = dado
+
+}
