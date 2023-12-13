5d9df5835680ec854a740a0666862deba68768fc
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-12-13 12:59:28 -0300

    continua refatoracao

--- main.js
+++ main.js
@@ -1,83 +1,54 @@
-import { zerarIguais, CalcPontosJog, CalcPontosBot, ganhaJog } from './src/DefinirDados.js';
+import { Jog, Bot, valorBot, valorJog } from './src/DefinirDados.js';
 
-//define as matrizes que vao guardar os valores dos dados
-let bot = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
-let jog = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
-//define as variaveis que vao guardar os valores dos dados
-let botAux = 0;
-let jogAux = 0;
-//define as variaveis que vao guardar os pontos dos jogadores
-let pontoJog = 0;
-let pontoJog1 = 0;
-let pontoJog2 = 0;
-let pontoJog3 = 0;
-let pontoBot = 0;
-let pontoBot1 = 0;
-let pontoBot2 = 0;
-let pontoBot3 = 0;
+let botJogou = true;
+let jogJogou = false;
+let dadoJog = 0;
+let dadoBot = 0;
 
+function JogarDadoJog(){
+    dadoJog = Jog();
+}
+function JogarDadoBot(){
+    dadoBot = Bot();
+}
 
 const btnJogDado = document.getElementById('jogDado');
-btnJogDado.addEventListener('click',Jog);
+btnJogDado.addEventListener('click',JogarDadoJog);
 const btnBotDado = document.getElementById('botDado');
-btnBotDado.addEventListener('click',Bot);
+btnBotDado.addEventListener('click',JogarDadoBot);
 const botoesBot = document.querySelectorAll(".tabelabot td")
 for (let i=0;i<botoesBot.length;i++)
 {
-        botoesBot[i].addEventListener('click',function(){valorBot(botoesBot[i].id,botAux)});
+        botoesBot[i].addEventListener('click',function(){cicloBot(botoesBot[i].id,dadoBot)});
 }
 const botoesJog = document.querySelectorAll(".tabelajog td")
 for (let j=0;j<botoesJog.length;j++)
 {
-        botoesJog[j].addEventListener('click',function(){valorJog(botoesJog[j].id,jogAux)});
+        botoesJog[j].addEventListener('click',function(){cicloJog(botoesJog[j].id,dadoJog)});
 }
 
-
-function Jog() { //funcao que define o valor do dado do player 1 aleatoriamente entre 1 e 6
-    jogAux = Math.floor(Math.random() * 6) + 1;
-    document.getElementById('jog1').innerText = jogAux
-}
-function Bot() { //funcao que define o valor do dado do player 2 aleatoriamente entre 1 e 6
-    botAux = Math.floor(Math.random() * 6) + 1;
-    document.getElementById('bot1').innerText = botAux
-}
-
-
-function valorBot(id, dado) { //ciclo de jogo do p2 em que é definido o valor na tabela do p2 e sao executadas as funções do ciclo
-    //primeiramente define-se na tabela se obtendo a posição pelo ID e verificando se não há itens na posição desejada
-    let linha = id.charAt(3);
-    let coluna = id.charAt(5);
-    if (bot[linha - 1][coluna - 1] == 0) {
-        bot[linha - 1][coluna - 1] = dado;
-    } else {
+function cicloBot(id,dado){
+    if (!botJogou)
+    {
+        valorBot(id,dado);
+        JogarDadoBot();
+        botJogou = true;
+        jogJogou = false;
+    }
+    else{
         return 0;
     }
-    document.getElementById(id).innerText = dado; //define o valor na tabela
-    zerarIguais(coluna-1,bot); //verifica se ha itens para ser zerados 
-    Bot(); //aleatoriza o proximo dado
-    CalcPontosBot(); //calcula os pontos do player 2 e os atualiza
-    ganhaJog(); //verifica se o jogo acabou e avisa quem ganhou
 }
-function valorJog(id, dado) { //ciclo de jogo do p1 em que é definido o valor na tabela do p1 e sao executadas as funções do ciclo
-    //primeiramente define-se na tabela se obtendo a posição pelo ID e verificando se não há itens na posição desejada
-    let linha = id.charAt(3);
-    let coluna = id.charAt(5);
-    if (jog[linha - 1][coluna - 1] == 0) {
-        jog[linha - 1][coluna - 1] = dado;
-    } else {
+
+function cicloJog(id,dado){
+    if (!jogJogou)
+    {
+        valorJog(id,dado);
+        JogarDadoJog();
+        jogJogou = true;
+        botJogou = false;
+    }
+    else{
         return 0;
     }
-    document.getElementById(id).innerText = dado; //define o valor na tabela
-    zerarIguais(coluna-1,jog); //verifica se ha itens para ser zerados
-    Jog(); //aleatoriza o proximo dado
-    CalcPontosJog(); //calcula os pontos do player 2 e os atualiza
-    ganhaJog(); //verifica se o jogo acabou e avisa quem ganhou
 }
--- src/DefinirDados.js
+++ src/DefinirDados.js
@@ -1,3 +1,69 @@
+//define as matrizes que vao guardar os valores dos dados
+let bot = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+let jog = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+//define as variaveis que vao guardar os valores dos dados
+let botAux = 0;
+let jogAux = 0;
+//define as variaveis que vao guardar os pontos dos jogadores
+let pontoJog = 0;
+let pontoJog1 = 0;
+let pontoJog2 = 0;
+let pontoJog3 = 0;
+let pontoBot = 0;
+let pontoBot1 = 0;
+let pontoBot2 = 0;
+let pontoBot3 = 0;
+
+function Jog() { //funcao que define o valor do dado do player 1 aleatoriamente entre 1 e 6
+    jogAux = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('jog1').innerText = jogAux
+    return jogAux;
+}
+function Bot() { //funcao que define o valor do dado do player 2 aleatoriamente entre 1 e 6
+    botAux = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('bot1').innerText = botAux
+    return botAux;
+}
+
+
+function valorBot(id, dado) { //ciclo de jogo do p2 em que é definido o valor na tabela do p2 e sao executadas as funções do ciclo
+    //primeiramente define-se na tabela se obtendo a posição pelo ID e verificando se não há itens na posição desejada
+    let linha = id.charAt(3);
+    let coluna = id.charAt(5);
+    if (bot[linha - 1][coluna - 1] == 0) {
+        bot[linha - 1][coluna - 1] = dado;
+    } else {
+        return 0;
+    }
+    document.getElementById(id).innerText = dado; //define o valor na tabela
+    //zerarIguais(coluna-1,bot); //verifica se ha itens para ser zerados 
+    Bot(); //aleatoriza o proximo dado
+    CalcPontosBot(); //calcula os pontos do player 2 e os atualiza
+    ganhaJog(); //verifica se o jogo acabou e avisa quem ganhou
+}
+function valorJog(id, dado) { //ciclo de jogo do p1 em que é definido o valor na tabela do p1 e sao executadas as funções do ciclo
+    //primeiramente define-se na tabela se obtendo a posição pelo ID e verificando se não há itens na posição desejada
+    let linha = id.charAt(3);
+    let coluna = id.charAt(5);
+    if (jog[linha - 1][coluna - 1] == 0) {
+        jog[linha - 1][coluna - 1] = dado;
+    } else {
+        return 0;
+    }
+    document.getElementById(id).innerText = dado; //define o valor na tabela
+    //zerarIguais(coluna-1,jog); //verifica se ha itens para ser zerados
+    Jog(); //aleatoriza o proximo dado
+    CalcPontosJog(); //calcula os pontos do player 2 e os atualiza
+    ganhaJog(); //verifica se o jogo acabou e avisa quem ganhou
+}
 function CalcPontosJog()
 {
     //calcula os pontos de cada coluna do jogador 1 e os atualiza
@@ -128,4 +194,4 @@ function zerarIguais(col,tipo){
         }
     }
 }
-export { zerarIguais, CalcPontosJog, CalcPontosBot, ganhaJog };
+export { Jog, Bot, valorBot, valorJog };
